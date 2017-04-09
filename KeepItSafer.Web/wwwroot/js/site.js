582eb54dd61e8298a85aaad5c7d5dcec0c5049ce
Andy Whitfield <andy@andywhitfield.me.uk>
2017-04-09 20:16:58 +0100

    Add JS to hide/show all password groups.

--- KeepItSafer.Web/wwwroot/js/site.js
+++ KeepItSafer.Web/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿
+﻿var groups = {};
+
+function initialisePasswordGroups() {
+    groups.groupsSection = $('#PasswordGroups');
+    $('section', groups.groupsSection).children('article').hide();
+    $('.group-section div', groups.groupsSection).click(toggleShowHideGroupItems);
+}
+
+function toggleShowHideGroupItems() {
+    var group = $(this);
+    console.log('group clicked: ' + group.parent().attr('id') + '; display=' + group.next().css('display'));
+    group.next().toggle();
+    $(window).scrollTop(group.position().top);
+}
